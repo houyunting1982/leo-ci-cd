0652fb43e797c5b033ba0e9837f5585f8aa3df68
Yunting Hou <yhou@servicetitan.com>
2021-10-15 22:33:10 -0700

    Add unit test

---
+++ Web.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using System;
+
+using Xunit;
+
+namespace Web.Tests {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+            Assert.True(1 == 1);
+        }
+    }
+}
