c536ac839042f588dc0a3104e0ea5eeca5cb56e3
Yunting Hou <yhou@servicetitan.com>
2021-10-16 17:41:52 -0700

    Update exception page

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -37,11 +37,11 @@ namespace Web {
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web v1"));
             }
-
+            app.UseDeveloperExceptionPage();
             app.UseHttpsRedirection();
 
             app.UseRouting();
